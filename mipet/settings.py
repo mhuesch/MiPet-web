0b55c93457214b10ff12f2f974cdad4f8b7ea276
Lizz B <huzzah.its.lizz@gmail.com>
2012-11-05 17:22:24 -0600

    added timelinejs folder and adjusted settings.py

--- mipet/settings.py
+++ mipet/settings.py
@@ -119,6 +119,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'gunicorn',
+    'timelinejs',              
     
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
