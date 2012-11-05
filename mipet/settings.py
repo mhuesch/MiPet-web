41e0c95b87cb362a674dc66c95f5479888dc34e7
Michael Hueschen <michaelhueschen@gmail.com>
2012-11-05 17:41:52 -0600

    Created timeline django app

--- mipet/settings.py
+++ mipet/settings.py
@@ -119,6 +119,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'gunicorn',
+    'timeline',
     #'timelinejs',
     
     # Uncomment the next line to enable admin documentation:
