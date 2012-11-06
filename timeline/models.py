750ff7c0e064b696114f7cda5729c1dd049114ed
Michael Hueschen <michaelhueschen@gmail.com>
2012-11-06 17:19:46 -0600

    Create database models

--- timeline/models.py
+++ timeline/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Pet(models.Model):
+    name = models.CharField(max_length=50)
+    joined_date = models.DateField()
+
+class Event(models.Model):
+    pet = models.ForeignKey(Pet)
+    moment = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=700)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    events = models.ManyToManyField(Event, related_name='tags')
+
+class Media(models.Model):
+    event = models.ForeignKey(Event)
+    url = models.CharField(max_length=2000)
+    credit = models.CharField(max_length=100)
+    caption = models.CharField(max_length=280)
+
+
