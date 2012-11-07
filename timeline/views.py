501a6fa4610a6f4eea9f9ad0a815c5776d14f077
Michael Hueschen <michaelhueschen@gmail.com>
2012-11-06 18:37:57 -0600

    Added Event API

--- timeline/views.py
+++ timeline/views.py
@@ -16,21 +16,18 @@ def index(request):
 def api_pet(request,id):
     try:
         pet_model = Pet.objects.get(id=id)
-    except DoesNotExist:
+    except Pet.DoesNotExist:
         return HttpResponse()
 
-    # Pet data
     response_data = dict()
+
+    # Pet data
     response_data['id'] = id
     response_data['name'] = pet_model.name
     
     # Convert date object to dict
     joined = pet_model.joined_date
-    join = dict()
-    join['year'] = joined.year
-    join['month'] = joined.month
-    join['day'] = joined.day
-    response_data['joined_date'] = join
+    response_data['joined_date'] = joined.isoformat()
 
     # Get pet event ids
     pet_events = Event.objects.filter(pet__id=id)
@@ -39,10 +36,23 @@ def api_pet(request,id):
     return HttpResponse(json.dumps(response_data), mimetype="application/json")
 
 def api_event(request,id):
-    
-    
+    try:
+        event_model = Event.objects.get(id=id)
+    except Event.DoesNotExist:
+        return HttpResponse()
+
     response_data = dict()
-    response_data['result'] = 'test'
+
+    # Event data
+    response_data['id'] = id
+    response_data['moment'] = event_model.moment.isoformat()
+    response_data['title'] = event_model.title
+    response_data['description'] = event_model.description
+
+    # Media
+    media_ids = Media.objects.filter(event__id=id)
+    response_data['media'] = map(lambda x: str(x), media_ids.values_list('id',flat=True))
+
     return HttpResponse(json.dumps(response_data), mimetype="application/json")
 
 def api_media(request,id):
