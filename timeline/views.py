b0d367508e0365009bb58b1640aad5e305cf4b98
Michael Hueschen <michaelhueschen@gmail.com>
2012-11-06 18:18:47 -0600

    Convert date object to dict

--- timeline/views.py
+++ timeline/views.py
@@ -23,7 +23,14 @@ def api_pet(request,id):
     response_data = dict()
     response_data['id'] = id
     response_data['name'] = pet_model.name
-    response_data['joined_date'] = pet_model.joined_date
+    
+    # Convert date object to dict
+    joined = pet_model.joined_date
+    join = dict()
+    join['year'] = joined.year
+    join['month'] = joined.month
+    join['day'] = joined.day
+     response_data['joined_date'] = join
 
     # Get pet event ids
     pet_events = Event.objects.filter(pet__id=id)
