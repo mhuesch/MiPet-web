c154c9c251bbd261957f5f0636f0c349e2c1abc2
Andrew Howes <AndrewHowes2013@u.northwestern.edu>
2012-11-06 18:16:20 -0600

    unicode methods

--- timeline/models.py
+++ timeline/models.py
@@ -4,12 +4,16 @@ from django.db import models
 class Pet(models.Model):
     name = models.CharField(max_length=50)
     joined_date = models.DateField()
+    def __unicode__(self):
+        return self.name
 
 class Event(models.Model):
     pet = models.ForeignKey(Pet)
     moment = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=700)
+    def __unicode__(self):
+        return u'%s did %s' % (self.pet.name, self.title)
 
 class Tag(models.Model):
     name = models.CharField(max_length=50)
@@ -20,5 +24,6 @@ class Media(models.Model):
     url = models.CharField(max_length=2000)
     credit = models.CharField(max_length=100)
     caption = models.CharField(max_length=280)
-
+    def __unicode__(self):
+        return u'Event: %s, Media: %s' % (self.event.title, self.caption)
 
