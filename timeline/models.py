7b9650b6c58eb145c18bd021cec34e6f3756b80a
Michael Hueschen <michaelhueschen@gmail.com>
2012-11-11 13:07:59 -0600

    Change unicode methods to work with API. Descriptions are not good right now.

--- timeline/models.py
+++ timeline/models.py
@@ -5,7 +5,7 @@ class Pet(models.Model):
     name = models.CharField(max_length=50)
     joined_date = models.DateField()
     def __unicode__(self):
-        return self.name
+        return u'%d' % self.id
 
 class Event(models.Model):
     pets = models.ManyToManyField(Pet, related_name='events')
@@ -13,7 +13,7 @@ class Event(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=700)
     def __unicode__(self):
-        return u'%s - %s' % (self.pet.name, self.title)
+        return u'%s' % self.id
 
 class Tag(models.Model):
     name = models.CharField(max_length=50)
@@ -25,5 +25,5 @@ class Media(models.Model):
     credit = models.CharField(max_length=100)
     caption = models.CharField(max_length=280)
     def __unicode__(self):
-        return u'Event: %s, Media: %s' % (self.event.title, self.caption)
+        return u'%s' % self.id
 
