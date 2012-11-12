b276909cce962c498caa71ca2f35b1e6f6b81af7
Andrew Howes <AndrewHowes2013@u.northwestern.edu>
2012-11-12 02:18:35 -0600

    make last backslash optional instead of removing it completely.
    Should have thought about that first.
    
    GET requests from rest_framework try pet/# without the trailing slash,
    otherwise I'd leave it just with the slash. Optional seems an okay
    compromise.

--- timeline/urls.py
+++ timeline/urls.py
@@ -4,11 +4,11 @@ from timeline import views
 
 urlpatterns = patterns('',
     url(r'^pet/$', views.PetList.as_view()),
-    url(r'^pet/(?P<pk>[0-9]+)$', views.PetDetail.as_view()),
+    url(r'^pet/(?P<pk>[0-9]+)/?$', views.PetDetail.as_view()),
     url(r'^event/$', views.EventList.as_view()),
-    url(r'^event/(?P<pk>[0-9]+)$', views.EventDetail.as_view()),
+    url(r'^event/(?P<pk>[0-9]+)/?$', views.EventDetail.as_view()),
     url(r'^media/$', views.MediaList.as_view()),
-    url(r'^media/(?P<pk>[0-9]+)$', views.MediaDetail.as_view())
+    url(r'^media/(?P<pk>[0-9]+)/?$', views.MediaDetail.as_view())
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
