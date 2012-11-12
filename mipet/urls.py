60ff51cc310a4d85733943490a4f9eb069df062d
Lizz B <huzzah.its.lizz@gmail.com>
2012-11-11 20:30:24 -0600

    Revert "/api/v1 now routes to the REST framework API"
    This reverts commit 70f4b18a24169ea818f7193fcd18a9f2419daacf.

--- mipet/urls.py
+++ mipet/urls.py
@@ -9,15 +9,14 @@ urlpatterns = patterns('',
     
     url(r'^$', 'timeline.views.index', name='index'),
     url(r'^profile/(?P<id>\d+)/$', 'timeline.views.timeline', name='profile'),
-    
-    #url(r'^api/v1/pet/(?P<id>\d+)/$', 'timeline.views.api_pet'),
-    #url(r'^api/v1/event/(?P<id>\d+)/$', 'timeline.views.api_event'),
-    #url(r'^api/v1/media/(?P<id>\d+)/$', 'timeline.views.api_media'),
+    url(r'^api/v1/pet/(?P<id>\d+)/$', 'timeline.views.api_pet'),
+    url(r'^api/v1/event/(?P<id>\d+)/$', 'timeline.views.api_event'),
+    url(r'^api/v1/media/(?P<id>\d+)/$', 'timeline.views.api_media'),
     url(r'^api/v1/timeline/(?P<id>\d+)/$', 'timeline.views.api_timeline', name='timeline'),
 
     # Django REST framework
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^api/v1/', include('timeline.urls')),
+    url(r'^rest/', include('timeline.urls')),
     
 
     # Uncomment the admin/doc line below to enable admin documentation:
