1d931c233ef000096772e71fb7d098939080702e
Michael Hueschen <michaelhueschen@gmail.com>
2012-11-11 23:27:22 -0600

    Created Backbone models and views

--- static/js/models/models.js
+++ static/js/models/models.js
@@ -2,9 +2,19 @@ window.Pet = Backbone.Model.extend({
 
     urlRoot:"/api/v1/pet",
 
+    defaults:{
+        myEvents: new EventCollection();
+    }
+
     // Called when generating a new instance of a model
-    initialize:function(){
-        
+    initialize:function () {
+        _.each(this.events, )
+
+    },
+
+    getEvent:function(id) {
+        var ev = new Event({id: id});
+        this.myEvents.add(ev);
     }
 });
 
@@ -13,7 +23,12 @@ window.Event = Backbone.Model.extend({
 
     urlRoot:"/api/v1/event",
 
-    initialize:function(){
+    initialize:function () {
 
     }
+});
+
+
+window.EventCollection = Backbone.Collection.extend({
+    model: Event
 });
--- static/js/views/views.js
+++ static/js/views/views.js
@@ -7,6 +7,42 @@ window.PetView = Backbone.View.extend({
     },
 
     render:function () {
-        var variables = 
+        $(this.el).html('<h2>This pet is named '+this.model.get('name')+'</h2>');
     }
-})
+});
+
+
+window.EventView = Backbone.View.extend({
+
+    tagName:"li",
+
+    initialize:function () {
+        _.bindAll(this, 'render');
+    },
+
+    render:function () {
+        $(this.el).html('<span>Title: '+this.model.get('title')+'</span>');
+        return this;
+    }
+});
+
+window.EventListView = Backbone.View.extend({
+
+    el: $('events'),
+
+    initialize:function () {
+        _.bindAll(this, 'render', 'appendEvent');
+        this.render();
+    },
+
+    render:function () {
+        var self = this;
+        $(this.el).append("<p>This is a list of events</p>");
+        $(this.el).append("<ul></ul>");
+        _(this.collection.)
+    }
+});
+
+
+
+
