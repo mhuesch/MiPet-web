6c3cae2df754356cb0e9a4d1885ce65de4a86c31
Andrew Howes <AndrewHowes2013@u.northwestern.edu>
2012-11-11 21:02:29 -0600

    Revert "Revert "Add relationship serializers""
    This reverts commit 94f4de4bfef55b2b103951a615725993ec03afba.

--- timeline/serializers.py
+++ timeline/serializers.py
@@ -2,14 +2,18 @@ from timeline.models import Pet, Event, Media
 from rest_framework import serializers
 
 class PetSerializer(serializers.HyperlinkedModelSerializer):
+    events = serializers.ManyRelatedField()
+
     class Meta:
         model = Pet
-        fields = ('name', 'joined_date')
+        fields = ('name', 'joined_date', 'events')
 
 class EventSerializer(serializers.HyperlinkedModelSerializer):
+    pet = serializers.RelatedField(source='pet')
+
     class Meta:
         model = Event
-        fields = ('moment', 'title', 'description')
+        fields = ('pet', 'moment', 'title', 'description')
 
 class MediaSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
