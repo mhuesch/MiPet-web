ab5f4f719e9c3f513ae71ccc9cb31a7fd0d7201e
Michael Hueschen <michaelhueschen@gmail.com>
2012-11-15 23:17:42 -0600

    Expose Event id in API

--- timeline/serializers.py
+++ timeline/serializers.py
@@ -1,6 +1,7 @@
 from timeline.models import Pet, Event, Media
 from rest_framework import serializers
 
+
 class PetSerializer(serializers.HyperlinkedModelSerializer):
     events = serializers.ManyPrimaryKeyRelatedField(source='events')
 
@@ -9,12 +10,13 @@ class PetSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('name', 'joined_date', 'bio', 'prof_pic', 'events')
 
 class EventSerializer(serializers.HyperlinkedModelSerializer):
+    event_id = serializers.IntegerField(source='id')
     pets = serializers.ManyPrimaryKeyRelatedField(source='pets')
     media = serializers.ManyPrimaryKeyRelatedField(source='media')
 
     class Meta:
         model = Event
-        fields = ('pets', 'moment', 'title', 'description', 'media')
+        fields = ('event_id', 'pets', 'moment', 'title', 'description', 'media')
 
 class MediaSerializer(serializers.HyperlinkedModelSerializer):
     event = serializers.ManyPrimaryKeyRelatedField(source='event')
