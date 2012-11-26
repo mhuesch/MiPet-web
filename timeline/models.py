48c9cb7be12514c1b7f8321523d1ffff122c8bbf
Michael Hueschen <michaelhueschen@gmail.com>
2012-11-25 19:55:56 -0600

    User model

--- timeline/models.py
+++ timeline/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from datetime import datetime, date
+from django.contrib.auth.models import User
 
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, unique=True)
+    bio = models.CharField(max_length=140)
+    prof_pic = models.URLField(blank=True, null=True)
+
+User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
 
 class Pet(models.Model):
     name = models.CharField(max_length=50)
