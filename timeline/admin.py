34e85aa52128a276107df492e9ff351001f3ae88
Michael Hueschen <michaelhueschen@gmail.com>
2012-11-25 20:13:30 -0600

    Register UserProfile with Admin

--- timeline/admin.py
+++ timeline/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from timeline.models import Pet, Event, Media
+from timeline.models import UserProfile, Pet, Event, Media
 
+admin.site.register(UserProfile)
 admin.site.register(Pet)
 admin.site.register(Event)
 admin.site.register(Media)
