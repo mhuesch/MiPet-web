0a07d99d6016f06f1b971968965980c13b285d17
Michael Hueschen <michaelhueschen@gmail.com>
2012-11-25 21:26:45 -0600

    Fix typo

--- timeline/serializers.py
+++ timeline/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 
 class UserProfileSerializer(serializers.HyperlinkedModelSerializer):
     pk = serializers.IntegerField(source='pk', read_only=True)
-    pets = serializers.ManyPrimaryKeyRelatedField(source=='pets')
+    pets = serializers.ManyPrimaryKeyRelatedField(source='pets')
 
     class Meta:
         model = UserProfile
