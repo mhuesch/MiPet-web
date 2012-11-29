808d64620a440a83ea337750a7dbec620a5946db
Michael Hueschen <michaelhueschen@gmail.com>
2012-11-28 23:21:50 -0600

    Register Milestone with admin

--- timeline/admin.py
+++ timeline/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from timeline.models import UserProfile, Pet, Event, Media
+from timeline.models import UserProfile, Pet, Event, Media, Milestone
 
 admin.site.register(UserProfile)
 admin.site.register(Pet)
 admin.site.register(Event)
 admin.site.register(Media)
+admin.site.register(Milestone)
